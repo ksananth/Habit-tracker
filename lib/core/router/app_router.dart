a21d2468fa60feb52993528e5fbb259ba2d62515
ananthks <ks.ananth1987@gmail.com>
2026-03-07 06:44:51 +0100

    Add DashboardScreen in Router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:go_router/go_router.dart';
+import 'package:habittracker/features/dashboard/presentation/screen/dasboard_screen.dart';
 import 'package:habittracker/features/splash/splash_screen.dart';
 import 'package:habittracker/features/home/presentation/screen/home_screen.dart';
 
 abstract class AppRoutes {
   static const splash = '/';
   static const home = '/home';
+  static const dashboard = '/dashboard';
 }
 
 final appRouter = GoRouter(
@@ -17,6 +19,10 @@ final appRouter = GoRouter(
     GoRoute(
       path: AppRoutes.home,
       builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: AppRoutes.dashboard,
+      builder: (context, state) => const DashboardScreen(),
     ),
   ],
 );
