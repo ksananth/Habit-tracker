5705ed706464a234aa6a806c439daf0d0dc0a012
ananthks <ks.ananth1987@gmail.com>
2026-03-09 15:34:14 +0100

    Extract strings to constant

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,32 @@
+abstract class AppStrings {
+  // Home
+  static const retry = 'Retry';
+  static const lastActivity = 'Last activity';
+  static const startFirstHabit = 'Start your first habit today!';
+  static const logActivity = 'Log Activity';
+
+  // Dashboard
+  static const myHabits = 'My Habits';
+  static const home = 'Home';
+  static const addNewHabit = 'Add new Habit';
+  static const noHabitsYet = 'No habits yet';
+  static const addHabitToStart = 'Add a habit to start tracking';
+
+  // Add Habit
+  static const addNewHabitTitle = 'Add New Habit';
+  static const habit = 'Habit';
+  static const selectAHabit = 'Select a habit';
+  static const startDate = 'Start Date';
+  static const selectStartDate = 'Select start date';
+  static const endDate = 'End Date';
+  static const selectEndDate = 'Select end date';
+  static const createHabit = 'Create Habit';
+  static const selectAHabitSheet = 'Select a Habit';
+
+  // Attendance
+  static const attendance = 'Attendance';
+  static const markActivityDescription =
+      'Mark your activity here to indicate that you performed your task today.';
+  static const alreadyAccomplished = 'Already accomplished today!';
+  static const accomplishToday = 'Yes, I accomplished this task today';
+}
